e4cf6e1c30328af4fe98c32107142f694a63a3dc
Lorens Cuscan <lorenscuscan@gmail.com>
2023-11-29 03:28:47 -0300

    fix

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -20,31 +20,28 @@
 	<link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
 	<!-- main css -->
 	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="/resources/demos/style.css">
 
 	<!-- scripts --> 
-	<script src="
-https://cdn.jsdelivr.net/npm/jquery-ui-slider@1.12.1/jquery-ui.min.js
-"></script>
-<link href="
-https://cdn.jsdelivr.net/npm/jquery-ui-slider@1.12.1/jquery-ui.min.css
-" rel="stylesheet">
-</head>
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+		<script src="
+	https://cdn.jsdelivr.net/npm/jquery-ui-slider@1.12.1/jquery-ui.min.js
+	"></script>
+	<link href="
+	https://cdn.jsdelivr.net/npm/jquery-ui-slider@1.12.1/jquery-ui.min.css
+	" rel="stylesheet">
+	</head>
 
 <body>
 <header class="header_area">
-<form class="form">
 <div class="language-toggle">
-    <form method="POST" action="{{ route('switchLanguage', 'en') }}">
-        @csrf
-        <button type="submit">English</button>
-    </form>
+    <label for="language-slider">Selecione o idioma:</label>
+    <div id="language-slider"></div>
+    <span id="selected-language">Português</span>
 </div>
 		<div class="main_menu">
 			<nav class="navbar navbar-expand-lg navbar-light">
-		
-
 				<div class="container">
-					
 					<!-- Brand and toggle get grouped for better mobile display -->
 					<a class="navbar-brand logo_h" href="index.html"><img src="img/logo.png" alt=""></a>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -66,4 +63,31 @@ https://cdn.jsdelivr.net/npm/jquery-ui-slider@1.12.1/jquery-ui.min.css
 			</nav>
 		</div>
 	</header>
+
+	<script>
+    $(function() {
+        let languages = ["Português", "Inglês"]; // Array de idiomas disponíveis
+        let selectedLanguageIndex = 0; // Índice do idioma selecionado (0 para Português, 1 para Inglês)
+
+        $("#language-slider").slider({
+            min: 0,
+            max: languages.length - 1,
+            value: selectedLanguageIndex,
+            slide: function(event, ui) {
+                $("#selected-language").text(languages[ui.value]);
+            },
+            stop: function(event, ui) {
+                // Quando o controle deslizante é solto, você pode adicionar a lógica para alternar o idioma
+                let selectedLanguage = languages[ui.value];
+                if (selectedLanguage === "Inglês") {
+                    // Lógica para alterar para inglês
+                  
+                } else {
+                    // Lógica para alterar para português ou outro idioma
+                    
+                }
+            }
+        });
+    });
+</script>
 	<!--================ End Header Area =================-->
